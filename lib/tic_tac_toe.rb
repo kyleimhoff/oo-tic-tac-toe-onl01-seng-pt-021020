122b27b6de3e8a25a53e37107a8915d9e066d31c
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-08 18:02:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,7 +70,7 @@ class TicTacToe
       win = nil
       WIN_COMBINATIONS.each do |arr|
          if @board[arr[0]] == @board[arr[1]] && @board[arr[1]] == @board[arr[2]] && position_taken?{arr[0]}
-           win = arr[0]
+           win = arr
       
            
          end
